9d1732905af02eccac6f7fd01b9cb153c9898f6e
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2022-07-20 14:04:47 +0000

    decoy chest

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -19,6 +19,7 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.AirBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -42,6 +43,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import org.slf4j.Logger;
 
+import java.util.Random;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -218,7 +220,34 @@ public class ExampleMod
                 ChestBlockEntity cbe = (ChestBlockEntity) b;
                 var textcomp = cbe.getCustomName();
                 if (textcomp != null) {
-                    LOGGER.info("INTERACTED WITH CHEST BLOCK - {}", textcomp.getString());
+                    String thestring = textcomp.getString();
+                    LOGGER.info("INTERACTED WITH CHEST BLOCK - '{}'", thestring);
+                    if (thestring.equals("pepechest")) {
+                        LOGGER.info("interactied with pepechest");
+                        Random rand = new Random();
+                        int randomInt = rand.nextInt(2);
+                        LOGGER.info("GOT NUMBER {}", randomInt);
+                        if (randomInt == 1) {
+                            // Spawn creeper
+                            LOGGER.info("SPAWNING CREEPER HAHA!");
+                            Level world = event.getWorld();
+                            // Remove chest from position
+                            // world.removeBlockEntity(bp); // makes an empty default chest for some reason
+                            world.setBlock(bp, Blocks.AIR.defaultBlockState(), Blocks.AIR.getId(Blocks.AIR.defaultBlockState())); // breaks the chest
+
+                            // Spawn creeper
+                            Creeper creeper = new Creeper(EntityType.CREEPER, event.getWorld());
+                            creeper.setPos(event.getPos().getX(), event.getPos().getY()+2, event.getPos().getZ());
+                            // CompoundTag nbt = creeper.serializeNBT();
+                            // nbt.putBoolean("powered", true);
+                            // creeper.deserializeNBT(nbt);
+                            creeper.setCustomNameVisible(true);
+                            TextComponent name = new TextComponent("Get naenae'd");
+                            creeper.setCustomName(name);
+                            world.addFreshEntity(creeper);
+                        }
+                    }
+                    // see if we spawn a creeper instead
                 }
             }
         }
